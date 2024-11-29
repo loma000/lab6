a9c8ded2d9d3ac7b6cb5d0b44fd8a69568f5968e
loma000 <dechartorn.som@gmail.com>
2024-11-29 15:50:35 +0700

    first

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    
+    int num[200];
+    int i=0;
+    int oddnum=0;
+    int evennum=0;
+    while(true){cout << "Enter an integer: ";
+        
+        cin>> num[i];
+        
+        if(num[i]==0){break;}i++;
+    }
+    
+    for(int j=0;j<i;j++){
+
+        
+        if(num[j]%2==0){evennum++;}else{oddnum++;}
+        
+        
+    }
+    cout << "#Even numbers = "<< evennum<<endl;
+    cout << "#Odd numbers = "<<oddnum;
     return 0;
 }
